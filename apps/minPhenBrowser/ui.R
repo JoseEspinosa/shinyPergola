dcb51b8f8e2b8d59414d33c3216382ef4a82b645
Jose Espinosa <kadomu@gmail.com>
2016-02-08 11:10:48 +0100

    Making tabs in the graphical interface

--- apps/minPhenBrowser/ui.R
+++ apps/minPhenBrowser/ui.R
@@ -28,42 +28,65 @@ library(shiny)
 # ))
 
 shinyUI(pageWithSidebar(
-  headerPanel("Behavioral browser"),
+#   headerPanel("Behavioral browser"),
+  
+  
+  headerPanel("Behavioral browser",
+              tags$head(tags$style(type="text/css", "label.radio { display: inline-block; }", ".radio input[type=\"radio\"] { float: none; }"),
+                        tags$style(type="text/css", "select { max-width: 200px; }"),
+                        tags$style(type="text/css", "textarea { max-width: 185px; }"),
+                        tags$style(type="text/css", ".jslider { max-width: 200px; }"),
+                        tags$style(type='text/css', ".well { max-width: 330px; }"),
+                        tags$style(type='text/css', ".span4 { max-width: 330px; }")) 
+  ),
+
   
   sidebarPanel(
-    sliderInput("windowsize", 
-                "Windowsize:", 
-                min = 1000,
-                max = 1000000,
-                value =1000,
-                step = 300),
-    uiOutput("bedGraphRange"),
-    uiOutput("idSelect"),
-    uiOutput("genomicPositionSelect"),
+    conditionalPanel(condition="input.tabs_p=='About'",
+                     h4("Introduction")
+    ),
+    conditionalPanel(condition="input.tabs_p=='Data upload'",
+                    sliderInput("windowsize", 
+                                "Windowsize:", 
+                                min = 1000,
+                                max = 1000000,
+                                value =1000,
+                                step = 300),
+                    uiOutput("bedGraphRange"),
+                    uiOutput("idSelect"),
+                    uiOutput("genomicPositionSelect"),
     
-    fileInput('fileEnv', 'Choose CSV File', multiple=TRUE, accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv', '.bed')),
-    tags$hr(),
-    checkboxInput('header', 'Header', FALSE),
-    radioButtons('sep', 'Separator', c(Comma=',', Semicolon=';', Tab='\t'), '\t'),
-    radioButtons('quote', 'Quote', c(None='', 'Double Quote'='"', 'Single Quote'="'"), '"'),
+      fileInput('fileEnv', 'Choose CSV File', multiple=TRUE, accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv', '.bed')),
+      tags$hr(),
+      checkboxInput('header', 'Header', FALSE),
+      radioButtons('sep', 'Separator', c(Comma=',', Semicolon=';', Tab='\t'), '\t'),
+      radioButtons('quote', 'Quote', c(None='', 'Double Quote'='"', 'Single Quote'="'"), '"'),
     
-    fileInput('filePhases', 'Phases CSV File', multiple=TRUE, accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv', '.bed')),
-    tags$hr(),
-    checkboxInput('header', 'Header', FALSE),
-    radioButtons('sep', 'Separator', c(Comma=',', Semicolon=';', Tab='\t'), '\t'),
-    radioButtons('quote', 'Quote', c(None='', 'Double Quote'='"', 'Single Quote'="'"), '"') 
-  ),
+      fileInput('filePhases', 'Phases CSV File', multiple=TRUE, accept=c('text/csv', 'text/comma-separated-values,text/plain', '.csv', '.bed')),
+      tags$hr(),
+      checkboxInput('header', 'Header', FALSE),
+      radioButtons('sep', 'Separator', c(Comma=',', Semicolon=';', Tab='\t'), '\t'),
+      radioButtons('quote', 'Quote', c(None='', 'Double Quote'='"', 'Single Quote'="'"), '"') 
+      )
+    ),
   
   mainPanel(
 #     textOutput("text1"),
-    plotOutput("intervals", height=800),
-    plotOutput("envInfo", height=20),
-    plotOutput("barPlotValue", height=400),
-    plotOutput("barPlotDuration", height=400),
-    plotOutput("barPlotN", height=400),
-    plotOutput("barPlotRate", height=400),
-    tableOutput('bed')
-#     tableOutput('bedgraph'),
-#     tableOutput('fileEnv')
+    tabsetPanel(
+      tabPanel("About",
+              HTML('<p>Pergola</p>')),
+      tabPanel("Data upload",               
+              plotOutput("intervals", height=800),
+              plotOutput("envInfo", height=20),
+              plotOutput("barPlotValue", height=400),
+              plotOutput("barPlotDuration", height=400),
+              plotOutput("barPlotN", height=400),
+              plotOutput("barPlotRate", height=400),
+              tableOutput('bed')),
+      id="tabs_p"
+#               tableOutput('bedgraph'),
+#               tableOutput('fileEnv')
+
   )
+)
 ))
